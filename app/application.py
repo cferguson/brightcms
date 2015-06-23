861956758aacd75333b3a5dfe015360ee5e73103
ctferguson <christopher.ferguson@brightscope.com>
2015-06-23 10:23:26 -0700

    Initial commit

---
+++ app/application.py
@@ -0,0 +1,3 @@
+@application.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
---
+++ run.py
@@ -0,0 +1,2 @@
+from app import application
+application.run(host='0.0.0.0', port=8080, debug=True)
